62f5eea9529196a83cfbc63ac137df532cfd5e81
yangyang <yy5223536@163.com>
2021-05-20 14:56:28 +0800

    master

--- DataType.c
+++ DataType.c
@@ -3,48 +3,55 @@
 
 struct a0
 {
-  char a;
+  char a; //1
 };
 
 struct a1
 {
-  int a;
+  int a;//4
 };
 
 struct a2
 {
-  short a;
+  short a;//4
 };
 
 struct a3
 {
-  float a;
+  float a;//8
 };
 
 struct a4
 {
-  double a;
+  double a;//8
 };
 
 struct a5
 {
-  int a;
-  char b;
-  short c;
+  int a;   //4
+  char b;  //此整数倍对齐，则此处为2
+  short c; //2
 };
 
 struct a6
 {
-  char a;
-  int b;
-  short c;
+  char a; //4
+  int b;  //4
+  short c;//4
 };
 
 struct a7
 {
-  char a;
-  int b;
-  double c;
+  char a; //4
+  int b;  //4
+  double c;//8
+};
+
+struct a8
+{
+  int a;  //4
+  char b; //4
+  int c;  //4
 };
 
 int main()
@@ -54,20 +61,13 @@ int main()
   float f = 5.3;
   double m = 12.65;
   double result = 0;
-  struct a0 stu_a0;
-  struct a1 stu_a1;
-  struct a2 stu_a2;
-  struct a3 stu_a3;
-  struct a4 stu_a4;
-  struct a5 stu_a5;
-  struct a6 stu_a6;
-  struct a7 stu_a7;
 
   printf(" sizeof(a) = %d, sizeof(c) = %d,sizeof(c) = %d,sizeof(m) = %d\n", sizeof(a), sizeof(c), sizeof(f), sizeof(m));
-  printf(" sizeof(struct a0) = %d, sizeof(struct a1) = %d\n", sizeof(stu_a0), sizeof(stu_a1) );
-  printf(" sizeof(struct a2) = %d, sizeof(struct a3) = %d\n", sizeof(stu_a2), sizeof(stu_a3) );
-  printf(" sizeof(struct a4) = %d, sizeof(struct a5) = %d\n", sizeof(stu_a4), sizeof(stu_a5) );
-  printf(" sizeof(struct a6) = %d, sizeof(struct a7) = %d\n", sizeof(stu_a6), sizeof(stu_a7) );
+  printf(" sizeof(struct a0) = %d, sizeof(struct a1) = %d\n", sizeof(struct a0), sizeof(struct a1) );
+  printf(" sizeof(struct a2) = %d, sizeof(struct a3) = %d\n", sizeof(struct a2), sizeof(struct a3) );
+  printf(" sizeof(struct a4) = %d, sizeof(struct a5) = %d\n", sizeof(struct a4), sizeof(struct a5) );
+  printf(" sizeof(struct a6) = %d, sizeof(struct a7) = %d\n", sizeof(struct a6), sizeof(struct a7) );
+  printf(" sizeof(struct a8) = %d, sizeof(struct a8) = %d\n", sizeof(struct a8), sizeof(struct a8) );
 
   //同类型数据间运算并输出结果
   printf("a + c = %d\n", a+c);//int转char
