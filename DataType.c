7b10eaefb08c56ef3956fa4669f567083335a855
yangyang <yy5223536@163.com>
2021-05-20 14:22:43 +0800

    master

--- DataType.c
+++ DataType.c
@@ -1,18 +1,50 @@
 #include "stdio.h"
 #include "string.h"
 
+struct a0
+{
+  char a;
+};
+
 struct a1
+{
+  int a;
+};
+
+struct a2
+{
+  short a;
+};
+
+struct a3
+{
+  float a;
+};
+
+struct a4
+{
+  double a;
+};
+
+struct a5
 {
   int a;
   char b;
   short c;
 };
 
-struct a2
+struct a6
 {
   char a;
   int b;
   short c;
+};
+
+struct a7
+{
+  char a;
+  int b;
+  double c;
 };
 
 int main()
@@ -22,11 +54,20 @@ int main()
   float f = 5.3;
   double m = 12.65;
   double result = 0;
+  struct a0 stu_a0;
   struct a1 stu_a1;
   struct a2 stu_a2;
+  struct a3 stu_a3;
+  struct a4 stu_a4;
+  struct a5 stu_a5;
+  struct a6 stu_a6;
+  struct a7 stu_a7;
 
   printf(" sizeof(a) = %d, sizeof(c) = %d,sizeof(c) = %d,sizeof(m) = %d\n", sizeof(a), sizeof(c), sizeof(f), sizeof(m));
-  printf(" sizeof(struct a1) = %d, sizeof(struct a2) = %d\n", sizeof(stu_a1), sizeof(stu_a2) );
+  printf(" sizeof(struct a0) = %d, sizeof(struct a1) = %d\n", sizeof(stu_a0), sizeof(stu_a1) );
+  printf(" sizeof(struct a2) = %d, sizeof(struct a3) = %d\n", sizeof(stu_a2), sizeof(stu_a3) );
+  printf(" sizeof(struct a4) = %d, sizeof(struct a5) = %d\n", sizeof(stu_a4), sizeof(stu_a5) );
+  printf(" sizeof(struct a6) = %d, sizeof(struct a7) = %d\n", sizeof(stu_a6), sizeof(stu_a7) );
 
   //同类型数据间运算并输出结果
   printf("a + c = %d\n", a+c);//int转char
