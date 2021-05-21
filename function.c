cd6b5a0bbeb2a44be0e3ff80075ef70535e90f69
yangyang <yy5223536@163.com>
2021-05-21 13:45:18 +0800

    master

--- function.c
+++ function.c
@@ -17,9 +17,26 @@ int use(int (*binfunc)(int a, int b), int a, int b)
   return (*binfunc)(a, b);
 }
 
+void func( int a )
+{
+  printf("Input of Data = %d\n", a);
+}
+
 int main()
 {
+  unsigned int ch;
+  void (*func_ptr)(int) = func;
+  int (*use_ptr[])(int,int) = (add, sub);
+
+  scanf("%d", &ch);
+  if( ch > 2 )
+    return 0;
+
   printf("%d\n", use(add,1,2));  
+  func_ptr(10);
+
+  (*use_ptr[ch])(1,5);
+
   return 0;
 }
 
