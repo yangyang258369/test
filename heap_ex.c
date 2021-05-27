93c1b3569370be50ac2bf25da88a5fca322222d4
yangyang <yy5223536@163.com>
2021-05-27 17:19:49 +0800

    master

--- heap_ex.c
+++ heap_ex.c
@@ -4,19 +4,27 @@
 int main()
 {
   int *p = (int*)malloc(20);
+  int *n = NULL;
+  int i = 0;
 
-  for( int i = 0; i < 20; i++ )
+  for( i = 0; i < 20; i++ )
   {
     p[i] = i;
     printf("p[%d] = %d\n", i, p[i]);
   }
 
   printf("int *p -> sizeof(p) = %d\n", sizeof(p));
-  printf("(int*)malloc(20) -> sizeof((int*)malloc(20)) = %d\n", sizeof((int*)malloc(20)));
+  printf("int *n -> sizeof(n) = %d\n", sizeof(n));
 
-
+  for ( i = 0; i < 20; i++)
+  {
+   printf("malloc[%d] = %s\n", i, p[i]);
+  }
 
   printf("hello,world!\n");
+
+  free(p);
+
   return 0;
 }
 
