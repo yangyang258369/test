15a3e02db0d84f9ac92073ddd6b07c0578e369c2
yangyang <yy5223536@163.com>
2021-06-01 15:34:07 +0800

    master

--- linear_allocator.c
+++ linear_allocator.c
@@ -1,10 +1,106 @@
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
+#include "string.h"
+#include "math.h"
+
+//大的内存块
+typedef struct linear_block
+{
+  /* data */
+  struct  linear_block *next;  
+}linear_block_t;
+
+//线性分配器
+typedef struct linear_allocator
+{
+  /* data */
+  linear_block_t *header;
+  unsigned char *end;
+  size_t blksz;
+}linear_allocator_t;
+
+//最小的块大小
+#define         LINEAR_MIN_BLOCK_SIZE      1024
+//内存块头大小
+#define         HEADER_SIZE                sizeof(linear_block_t)
+//取内存块的Buffer
+#define         LINEAR_BUFFER(b)           ((unsigned char*)(b) + HEADER_SIZE)
+
+//初始化
+static void linear_init( linear_allocator_t *a, size_t size )
+{
+  a->blksz = size + HEADER_SIZE < LINEAR_MIN_BLOCK_SIZE ? LINEAR_MIN_BLOCK_SIZE : size + HEADER_SIZE;
+  a->header = NULL;
+  a->end = (unsigned char*)HEADER_SIZE;   //此举是为了在分配时减少一次判断
+}
+
+//分配内存器
+static inline linear_block_t* _linear_new_block( size_t size )
+{
+  return (linear_block_t*)malloc(size);
+}
+
+//分配内存
+static void* linear_allc( linear_allocator_t *a, size_t size )
+{
+  if(a->end - LINEAR_BUFFER(a->header) < size)//内存不足，分配新的内存块
+  {
+      if( size + HEADER_SIZE > a->blksz )
+      {
+        //如果请求的大小比默认内存块大，则尝试创建更大的内存块
+        linear_block_t *block = _linear_new_block(HEADER_SIZE + size);
+        unsigned char *buffer = LINEAR_BUFFER(block);
+        if( a->header != NULL )
+        {
+          block->next = a->header->next;
+          a->header->next = block;
+        }
+        else
+        {
+          //如果头结点不存在，则成为头结点
+          block->next = NULL;
+          a->header = block;
+          a->end = buffer;
+        }
+        return buffer;
+      }
+      else
+      {
+        //否则正常分配内存块，并成为链表头
+        linear_block_t *block = _linear_new_block(a->blksz);
+        block->next = a->header;
+        a->header = block;
+        a->end = (unsigned char*)block + a->blksz;
+      }
+  }
+  a->end -= size;
+  return a->end;
+}
+
+//终止
+static void linear_term( linear_allocator_t *a )
+{
+  linear_block_t *block = a->header;
+  linear_block_t *temp;
+
+  while(block)
+  {
+    temp = block;
+    block = block->next;
+    free(temp);
+  }
+}
 
 int main()
 {
+  //linear_init();
+
   printf("hello,world!\n");
   return 0;
 }
+
+
+
 
 
---
+++ list_node.c
@@ -0,0 +1,40 @@
+
+#include "stdio.h"
+#include "string.h"
+
+struct list_node
+{
+  /* data */
+  int data;                //数据域，用于存储数据
+  struct list_node *next;  //指针，可以用来访问节点数据，也可以遍历，指向下一个节点
+};
+
+typedef struct list_node list_single;
+
+list_single *create_list_node( int data )
+{
+  list_single *node = NULL;                              //首先，定义一个头指针
+  node = (list_single*)malloc(sizeof(list_single));      //分配内存空间
+  if( NULL == node )
+  {
+    printf("malloc fail!\n");
+  }
+
+  memset(node, 0, sizeof(list_single));                  //请客内存空间
+  node->data = data;                                     //给链表节点的数据赋值
+  node->next = NULL;                                     //将链表的指针域指向空
+
+  return node;
+}
+
+int main()
+{
+  int data = 100;
+
+  list_single *node_ptr = create_list_node(data); //创建一个节点
+  printf("node_ptr->data = %d\n", node_ptr->data);
+  printf("node_ptr->next = %d\n", node_ptr->next);
+
+  free(node_ptr);
+  return 0;
+}
